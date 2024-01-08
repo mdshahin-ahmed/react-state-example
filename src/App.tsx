3bd4af38746142d542db5ef655668c9985659c1f
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-01-08 21:34:03 +0600

    example counter with class

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CounterWithClass from "./components/CounterWithClassComponent";
+
+function App() {
+  return (
+    <div>
+      <CounterWithClass />
+    </div>
+  );
+}
+
+export default App;
